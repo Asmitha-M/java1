8836f4c46619e16d214f8ee3aa7efa173d02f8cd
Asmitha-M <asmitha.23cs020@sode-edu.in>
2024-09-09 23:58:50 +0530

    Create J2.java

---
+++ J2.java
@@ -0,0 +1,26 @@
+public class J2{
+    public static void main(String[] args) {
+      
+      for(int i=65;i<=90;i++)
+      {
+        System.out.print(" "+i );
+      }
+      System.out.println(" ");
+      for(int i=97;i<=112;i++)
+      {
+        System.out.print(" "+i );
+      }
+      System.out.println(" ");
+     
+      for(int j=65;j<=90;j++)
+      {
+        System.out.print(" "+(char)j);
+      }
+      System.out.println(" ");
+      for(int j=97;j<=122;j++)
+      {
+        System.out.print(" "+(char)j);
+      }
+    }
+    
+  }
