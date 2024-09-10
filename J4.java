60e259ab37790be012239994f4fae58511dacaba
Asmitha-M <asmitha.23cs020@sode-edu.in>
2024-09-10 15:20:03 +0530

    Create J4.java

---
+++ J4.java
@@ -0,0 +1,27 @@
+public class J4{
+
+  public static void main(String[] args){
+
+    String name1="ASMITHA";
+
+    String name2=name1.substring(4,5);
+
+    String name3=name1.substring(3,4);
+
+    String name4=name1.substring(2,3);
+
+    String name5=name1.substring(1,2);
+
+    String name6=name1.substring(0,1);
+
+    System.out.println(name2+""+name3+""+name4+""+name5+""+name6);
+
+    System.out.println(name3);
+
+    System.out.println(name4);
+
+    System.out.println(name5);
+
+    System.out.println(name6);
+}
+}
